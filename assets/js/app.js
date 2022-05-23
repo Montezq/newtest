5eee1b441792bc5e28f88e6802a6fdd63776d443
Monte <intalmaci21@gmail.com>
2022-05-24 01:44:28 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+(function(){
+    
+})()
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbigpe1xyXG4gICAgXHJcbn0pKCkiXX0=
